7afbfb2a7246f595f782d36878dccdea275decd0
charles.ausejojr <charles.ausejojr@gmail.com>
2024-07-20 20:21:33 +0800

    Modify and refactor test. Add fixtures refactoring.

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,30 +1,33 @@
 import random
 from playwright.sync_api import Page, expect # Synchronous API
+from pages.search import DuckDuckGoSearchPage
+from pages.result import DuckDuckGoResultPage
 
-HOME="https://www.duckduckgo.com"
 
 # --headed: to see browser
 # --headless: to NOT see browser (faster)
 
 # Accepts Page object
-def test_basic_duckduckgo_search(page : Page) -> None:
+def test_basic_duckduckgo_search(
+    page : Page,
+    search_page : DuckDuckGoSearchPage,
+    result_page : DuckDuckGoResultPage
+    ) -> None:
+
     search_input_list = ["Kendrick", "Python", "Spongebob"]
-    SEARCHBOX_INP = "#searchbox_input"
-    SEARCH_FORM_INP = "#search_form_input"
-    BTN = "Search" # aria-label
-    RESULTS_LIST = 'li[data-layout="organic"]'
     SEARCH_INP = random.choice(search_input_list)
+
     # Given the DuckDuckGo home page is displayed
-    page.goto(HOME)
+    search_page.load()
+
     # When the user searches for a phrase
-    page.locator(SEARCHBOX_INP).fill(SEARCH_INP)
-    page.get_by_role("button").all()[1].click()
+    search_page.search(SEARCH_INP)
+
     # Then the search result query is the phrase
-    expect(page.locator(SEARCH_FORM_INP)).to_have_value(SEARCH_INP)
+    expect(result_page.search_input).to_have_value(SEARCH_INP)
+
     # And the search result links pertain to the phrase
-    page.locator(RESULTS_LIST).nth(4).wait_for()
-    titles = page.locator(RESULTS_LIST).all_text_contents()
-    matches = [t for t in titles if SEARCH_INP.lower() in t.lower()]
-    assert len(matches) > 0
+    assert result_page.result_link_titles_contain_phrase(SEARCH_INP)
+
     # And the search result title contains the phrase
     expect(page).to_have_title(f"{SEARCH_INP} at DuckDuckGo")
